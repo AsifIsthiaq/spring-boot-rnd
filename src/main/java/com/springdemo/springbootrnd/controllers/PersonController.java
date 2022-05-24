e57280b14884f8b91df76f7facb87a9e4411aee2
Asif Isthiaq <isthiaq@tigerit.com>
2022-05-24 21:45:46 +0600

    Remove unnecessary comments

--- src/main/java/com/springdemo/springbootrnd/controllers/PersonController.java
+++ src/main/java/com/springdemo/springbootrnd/controllers/PersonController.java
@@ -12,7 +12,6 @@ import java.util.UUID;
 @RestController
 public class PersonController {
     private final PersonService personService;
-    // tesing token
 
     @Autowired
     public PersonController(PersonService personService) {
