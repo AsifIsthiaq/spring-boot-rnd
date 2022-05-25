e9c6d778f864d71f6a83bc47fcc37dd77298a288
Asif Isthiaq <isthiaq@tigerit.com>
2022-05-25 14:28:11 +0600

    Add mechanism to connect to postgres db Add method to get all the persons from person table from postgres demodb

---
+++ src/main/java/com/springdemo/springbootrnd/dao/PersonDataAccessService.java
@@ -0,0 +1,50 @@
+package com.springdemo.springbootrnd.dao;
+
+import com.springdemo.springbootrnd.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository("postgres")
+public class PersonDataAccessService implements PersonDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public int insertPerson(UUID id, Person person) {
+        return 0;
+    }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        final String sql = "SELECT id, name FROM person";
+        return jdbcTemplate.query(sql, (resultSet, i) -> {
+            UUID id = UUID.fromString(resultSet.getString("id"));
+            String name = resultSet.getString("name");
+            return new Person(id, name);
+        });
+    }
+
+    @Override
+    public Optional<Person> selectPersonById(UUID id) {
+        return null;
+    }
+
+    @Override
+    public int deletePersonById(UUID id) {
+        return 0;
+    }
+
+    @Override
+    public int updatePersonById(UUID id, Person updatedPerson) {
+        return 0;
+    }
+}
--- src/main/java/com/springdemo/springbootrnd/services/PersonService.java
+++ src/main/java/com/springdemo/springbootrnd/services/PersonService.java
@@ -16,7 +16,7 @@ public class PersonService {
     private final PersonDao personDao;
 
     @Autowired
-    public PersonService(@Qualifier("fakeDao") PersonDao personDao) {
+    public PersonService(@Qualifier("postgres") PersonDao personDao) {
         this.personDao = personDao;
     }
 
