dc6d969f9c7b33f49f42ead5b6a4f7169771dc1c
Asif Isthiaq <isthiaq@tigerit.com>
2022-05-28 11:19:36 +0600

    Remove unnecessary imports from HealthCheckController

--- src/main/java/com/springdemo/springbootrnd/controllers/HealthCheckController.java
+++ src/main/java/com/springdemo/springbootrnd/controllers/HealthCheckController.java
@@ -1,14 +1,10 @@
 package com.springdemo.springbootrnd.controllers;
 
-import com.springdemo.springbootrnd.models.Person;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
 
 @RequestMapping("health")
 @RestController
