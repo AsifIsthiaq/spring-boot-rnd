d2aa75968c51a9434376d18bbdcd11ee456686c3
Asif Isthiaq <isthiaq@tigerit.com>
2022-05-28 11:18:33 +0600

    Replace explicit type argument String,String with <>

--- src/main/java/com/springdemo/springbootrnd/controllers/HealthCheckController.java
+++ src/main/java/com/springdemo/springbootrnd/controllers/HealthCheckController.java
@@ -15,7 +15,7 @@ import java.util.UUID;
 public class HealthCheckController {
     @GetMapping()
     public HashMap<String,String> getPersonById() {
-        return new HashMap<String,String>() {{
+        return new HashMap<>() {{
             put("healthCheck","success");
         }};
     }
