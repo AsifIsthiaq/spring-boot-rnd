0ad3da0cee1559951678430fd2fe388f5edb5eea
Asif Isthiaq <isthiaq@tigerit.com>
2022-06-13 16:01:39 +0600

    Integrate swagger-ui using springdoc-openapi java library

--- src/main/java/com/springdemo/springbootrnd/SpringBootRndApplication.java
+++ src/main/java/com/springdemo/springbootrnd/SpringBootRndApplication.java
@@ -1,9 +1,12 @@
 package com.springdemo.springbootrnd;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Persons API", version = "1.0", description = "Persons Information"))
 public class SpringBootRndApplication {
 
     public static void main(String[] args) {
