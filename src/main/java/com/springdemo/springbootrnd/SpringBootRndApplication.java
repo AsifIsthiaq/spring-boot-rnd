aca5a873106f67689bb014c01aef8d2411446885
AsifIsthiaq <asifisthiaq@gmail.com>
2022-12-21 18:09:56 +0600

    Fix unable to run application using elephant sql postgres Fix datasource related build issues

--- src/main/java/com/springdemo/springbootrnd/SpringBootRndApplication.java
+++ src/main/java/com/springdemo/springbootrnd/SpringBootRndApplication.java
@@ -4,16 +4,19 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @OpenAPIDefinition(info = @Info(title = "Spring Boot API", version = "1.0.1", description = "This is a Demo Application built using Spring Boot & Postgres."))
 @EnableCaching
+//@ComponentScan(basePackages = {"com.springdemo.springbootrnd.services", "com.springdemo.springbootrnd.dao.user"})
+//@ComponentScan(basePackages = "com.springdemo.springbootrnd.services")
 public class SpringBootRndApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootRndApplication.class, args);
     }
-
 }
 
 /**
